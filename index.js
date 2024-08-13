eeffec2ce1fe2b14dbdb61b049f900c0c71968cb
skearney <skearney@akamai.com>
2024-08-13 14:19:53 -0500

    pushing out to change branches

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  const Enforcer = require('openapi-enforcer')
+
+  async function run () {
+  const [openapi, error, warning] = await Enforcer('./path/to/openapi.yml', {
+    fullResult: true
+  })
+  if (error !== undefined) console.error(error)
+  if (warning !== undefined) console.warn(warning)
+  if (openapi !== undefined) console.log('Document is valid')
+  }
+}
+
+catch {
+  run.catch(console.error)
 }
